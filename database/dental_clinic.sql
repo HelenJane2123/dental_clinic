1e4a4ab89f838585f7d921888cd47e58be1de414
unknown <HelenJane2123>
2024-11-13 19:01:49 +0800

    push updates

--- database/dental_clinic.sql
+++ database/dental_clinic.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 18, 2024 at 07:27 AM
+-- Generation Time: Nov 13, 2024 at 12:00 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -50,8 +50,7 @@ CREATE TABLE `accounts` (
 --
 
 INSERT INTO `accounts` (`id`, `member_id`, `firstname`, `lastname`, `contactnumber`, `termscondition`, `username`, `password`, `user_type`, `email`, `gender`, `address`, `profile_picture`, `remarks`, `date_created`) VALUES
-(11, 'M-5220', 'Heart Jasmine', 'Manalo', 2147483647, 1, 'HeartJasmine', '$2y$10$qsH.xioIYh2iBqUycFzfped/hmxA/iJEp41/cPd8ZN8TA0uAGqcJ2', 'user', 'manalohelenjane@gmail.com', '', '', '', '', '2024-10-09'),
-(12, 'M-4034', 'Heart Jasmine', 'Manalo', 2147483647, 0, 'HeartAdmin', '$2y$10$9VaOEL7XykS.us9j9NZmhOK/f4sbo2PLtW1MJRdsWYQvbFPre/Zrq', 'super_admin', 'manaloheartjasmine@gmail.com', '', '', '', '', '2024-10-15');
+(12, 'M-4034', 'Roselle', 'Santander', 2147483647, 0, 'RoselleSantander', '$2y$10$9VaOEL7XykS.us9j9NZmhOK/f4sbo2PLtW1MJRdsWYQvbFPre/Zrq', 'super_admin', 'manalohelenjane@gmail.com', 'female', 'for testing address', '', 'address', '2024-10-15');
 
 -- --------------------------------------------------------
 
@@ -69,16 +68,12 @@ CREATE TABLE `appointments` (
   `notes` text DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `canceled_at` datetime DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL
+  `updated_at` datetime DEFAULT NULL,
+  `updated_by` varchar(255) NOT NULL,
+  `canceled_by` varchar(255) NOT NULL,
+  `approved_by` varchar(255) NOT NULL,
+  `rescheduled_by` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `appointments`
---
-
-INSERT INTO `appointments` (`id`, `patient_id`, `appointment_date`, `appointment_time`, `services`, `status`, `notes`, `created_at`, `canceled_at`, `updated_at`) VALUES
-(31, 38, '2024-10-16', '10:00:00', 'brace_adjustment', 'Canceled', '', '2024-10-16 01:36:22', NULL, NULL),
-(32, 38, '2024-10-18', '09:30:00', 'brace_consultation', 'Canceled', '', '2024-10-16 07:04:42', NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -94,6 +89,30 @@ CREATE TABLE `consultations` (
   `previous_dentist` varchar(100) DEFAULT NULL,
   `last_dental_visit` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `doctors`
+--
+
+CREATE TABLE `doctors` (
+  `doctor_id` int(11) NOT NULL,
+  `account_id` int(11) NOT NULL,
+  `first_name` varchar(255) NOT NULL,
+  `last_name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `contact_number` varchar(20) DEFAULT NULL,
+  `specialty` varchar(255) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `doctors`
+--
+
+INSERT INTO `doctors` (`doctor_id`, `account_id`, `first_name`, `last_name`, `email`, `contact_number`, `specialty`, `created_at`) VALUES
+(1, 12, 'Roselle', 'Santander', 'helenjanemanalo@gmail.com', '123-456-7890', 'Dentist', '2024-11-08 08:36:50');
 
 -- --------------------------------------------------------
 
@@ -144,6 +163,23 @@ CREATE TABLE `medical_history` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `notifications`
+--
+
+CREATE TABLE `notifications` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `notif_to` int(11) NOT NULL,
+  `message` text NOT NULL,
+  `type` varchar(255) NOT NULL,
+  `is_read` tinyint(1) DEFAULT 0,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `patients`
 --
 
@@ -162,15 +198,9 @@ CREATE TABLE `patients` (
   `cellphone_no` varchar(20) DEFAULT NULL,
   `email` varchar(100) DEFAULT NULL,
   `home_address` varchar(255) DEFAULT NULL,
-  `occupation` varchar(100) DEFAULT NULL
+  `occupation` varchar(100) DEFAULT NULL,
+  `assigned_doctor` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `patients`
---
-
-INSERT INTO `patients` (`patient_id`, `member_id`, `last_name`, `first_name`, `middle_name`, `birthdate`, `age`, `sex`, `nickname`, `religion`, `nationality`, `cellphone_no`, `email`, `home_address`, `occupation`) VALUES
-(38, 'M-5220', 'Manalo', 'Heart Jasmine', 'Relox', '2004-02-05', 20, 'F', 'Heart', 'Born Again', 'Filipino', '09666988117', 'hjasminermanalo@gmil.com', 'Imus City Cavite', 'Student');
 
 --
 -- Indexes for dumped tables
@@ -196,6 +226,13 @@ ALTER TABLE `consultations`
   ADD KEY `patient_id` (`patient_id`);
 
 --
+-- Indexes for table `doctors`
+--
+ALTER TABLE `doctors`
+  ADD PRIMARY KEY (`doctor_id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
 -- Indexes for table `guardians`
 --
 ALTER TABLE `guardians`
@@ -208,6 +245,12 @@ ALTER TABLE `guardians`
 ALTER TABLE `medical_history`
   ADD PRIMARY KEY (`history_id`),
   ADD KEY `patient_id` (`patient_id`);
+
+--
+-- Indexes for table `notifications`
+--
+ALTER TABLE `notifications`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `patients`
@@ -223,37 +266,49 @@ ALTER TABLE `patients`
 -- AUTO_INCREMENT for table `accounts`
 --
 ALTER TABLE `accounts`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT for table `appointments`
 --
 ALTER TABLE `appointments`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=64;
 
 --
 -- AUTO_INCREMENT for table `consultations`
 --
 ALTER TABLE `consultations`
-  MODIFY `consultation_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `consultation_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT for table `doctors`
+--
+ALTER TABLE `doctors`
+  MODIFY `doctor_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT for table `guardians`
 --
 ALTER TABLE `guardians`
-  MODIFY `guardian_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `guardian_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT for table `medical_history`
 --
 ALTER TABLE `medical_history`
-  MODIFY `history_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `history_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `notifications`
+--
+ALTER TABLE `notifications`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=82;
 
 --
 -- AUTO_INCREMENT for table `patients`
 --
 ALTER TABLE `patients`
-  MODIFY `patient_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
+  MODIFY `patient_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=49;
 
 --
 -- Constraints for dumped tables
